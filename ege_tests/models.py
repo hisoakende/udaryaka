6f8368ebe89901136a406cdefce925dfda0d5264
Hisoakende <dimaivanitskii@gmail.com>
2021-11-20 14:12:06 +0500

    created base_template.html, created base of homepage, created model WordFromDictionary,connected postgresql

--- ege_tests/models.py
+++ ege_tests/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class WordFromDictionary(models.Model):
+    """Слово, которое будет добавлено в тест по ударениям"""
+    PART_OF_SPEECH = (
+        ('noun', 'Имя существительное'),
+        ('adjective', 'Имя прилагательное'),
+        ('verb', 'Глагол'),
+        ('participle', 'Причастие'),
+        ('gerunds', 'Деепричастие'),
+        ('adverb', 'Наречие')
+    )
+
+    word = models.CharField(verbose_name='Слово', max_length=50)
+    accented_character = models.IntegerField(verbose_name='Буква под ударением (по счету)')
+    part_of_speech = models.CharField(verbose_name='Часть речи', max_length=10, choices=PART_OF_SPEECH)
+
+    class Meta:
+        ordering = ['word']
+        verbose_name = 'Слово для тестов'
+        verbose_name_plural = 'Слова для тестов'
+
+    def __str__(self):
+        return self.word
--- ege_tests/urls.py
+++ ege_tests/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from .views import *
+
 urlpatterns = [
+    path('', Homepage.as_view(), name='homepage')
 ]
